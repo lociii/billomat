c4d42ef29e8dce95efc585df1690816e03eebcf9
Jens Nistler <opensource@jensnistler.de>
2015-07-09 12:35:17 +0200

    bump to 0.1.19

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='billomat',
     packages=find_packages(),
-    version='0.1.18',
+    version='0.1.19',
     description='billomat.com API client',
     author='Jens Nistler',
     author_email='opensource@jensnistler.de',
